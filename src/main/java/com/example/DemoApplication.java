5532a5308abfc4738e06eaaf0dd0a741f048d4a7
wangqian <wangqian@antiy.cn>
2019-08-28 14:57:08 +0800

    修正druid配置

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.example.dao")
 @SpringBootApplication
-public class SpringbootPlusDemoApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootPlusDemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
