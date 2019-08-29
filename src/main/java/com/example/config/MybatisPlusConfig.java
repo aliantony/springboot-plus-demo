bc98c0f61148dfe31f4357c4e937b95527bcc9c2
wangqian <wangqian@antiy.cn>
2019-08-29 14:46:18 +0800

    集成分页功能

--- src/main/java/com/example/config/MybatisPlusConfig.java
+++ src/main/java/com/example/config/MybatisPlusConfig.java
@@ -1,5 +1,7 @@
 package com.example.config;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,4 +22,14 @@ public class MybatisPlusConfig {
     public MyLogicSqlInjector myLogicSqlInjector() {
         return new MyLogicSqlInjector();
     }
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        // 开启 count 的 join 优化,只针对 left join !!!
+        return new PaginationInterceptor().setCountSqlParser(new JsqlParserCountOptimize(true));
+    }
+
 }
---
+++ src/main/java/com/example/page/Children.java
@@ -0,0 +1,14 @@
+package com.example.page;
+
+import lombok.Data;
+
+/**
+ * @author miemie
+ * @since 2019-06-12
+ */
+@Data
+public class Children {
+    private Long id;
+    private String name;
+    private Long userId;
+}
