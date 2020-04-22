f5340fbe37ec80540c0f166bfd40c5d9bf22e1fc
wangqian <wangqian@antiy.cn>
2020-04-22 11:11:35 +0800

    hutool

---
+++ src/main/java/com/example/spring/MyMain.java
@@ -0,0 +1,16 @@
+package com.example.spring;
+
+import cn.hutool.core.util.IdUtil;
+
+/**
+ * @program demo1
+ * @description 
+ * @author wangqian
+ * created on 2020-04-22
+ * @version  1.0.0
+ */
+public class MyMain {
+    public static void main(String[] args) {
+        System.out.println(IdUtil.simpleUUID());
+    }
+}
